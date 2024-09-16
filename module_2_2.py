8e0a8e11454f0979b777f0f92a4b69cf855287da
alve071211 <alve071211@gmail.com>
2024-09-16 13:29:03 +1200

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first=2025
+second=353
+third=80
+if first==second and second==third:
+    print('3')
+elif first==second or first==third or second==first or second==third:
+    print('2')
+else:
+    print('0')
